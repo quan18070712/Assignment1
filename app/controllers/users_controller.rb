5a25d6f12239c4e4c5c03e0a2f39a5703f4d5053
quan18070712 <quan.nguyen_bk2017@hcmut.edu.vn>
2020-08-07 11:12:26 +0700

    first work commit

---
+++ app/controllers/users_controller.rb
@@ -0,0 +1,45 @@
+class UsersController < ApplicationController
+  def show
+    @user = User.find(params[:id])
+  end
+
+  def new
+    @user = User.new
+  end
+
+  def create
+    @user = User.new(user_params) # Not the final implementation!
+    if @user.save
+      log_in @user
+      # Handle a successful save.
+      flash[:success] = "Welcome to the Sample App!"
+      redirect_to @user
+    else
+      render "new"
+    end
+  end
+
+  def update
+    @user = User.find(params[:id])
+    if @user.update(user_params)
+      # Handle a successful update.
+      flash[:success] = "Profile updated"
+      redirect_to @user
+    else
+      render "edit"
+    end
+  end
+
+  def edit
+    @user = User.find(params[:id])
+  end
+
+  private
+
+  def user_params
+    params.require(:user).permit(:name,
+                                 :email,
+                                 :password,
+                                 :password_confirmation)
+  end
+end
---
+++ config/routes.rb
@@ -0,0 +1,23 @@
+Rails.application.routes.draw do
+  
+  get 'sessions/new'
+  get 'users/new'
+  # root 'users#index'
+  root 'static_pages#home'
+  get '/home',      to: 'static_pages#home'
+  get '/help',      to: 'static_pages#help', as: 'helf'
+  get '/about',     to: 'static_pages#about'
+  get '/contact',   to: 'static_pages#contact'
+  get '/signup',    to: 'users#new'
+  get '/login',     to: 'sessions#new'
+  post '/login',    to: 'sessions#create'
+  get '/logout', to: 'sessions#destroy'
+  resources :users
+
+  # get 'static_pages/home'
+  # get 'static_pages/help'
+  # get 'static_pages/about'
+  # get 'static_pages/contact'
+  
+  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+end
