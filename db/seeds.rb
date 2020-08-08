d894b4571c4e6dde5b910e88fa6da2da09cf9637
quan18070712 <quan.nguyen_bk2017@hcmut.edu.vn>
2020-08-08 14:31:36 +0700

    Third on Saturday

--- db/seeds.rb
+++ db/seeds.rb
@@ -13,12 +13,13 @@ User.create!(name: "Example User",
              password_confirmation: "foobar",
              admin: true)
 # Generate a bunch of additional users.
-99.times do |n|
-  name = Faker::Name.name
-  email = "example-#{n + 1}@railstutorial.org"
-  password = "password"
-  User.create!(name: name,
-               email: email,
-               password: password,
-               password_confirmation: password)
-end
+
+# 99.times do |n|
+#   name = Faker::Name.name
+#   email = "example-#{n + 1}@railstutorial.org"
+#   password = "password"
+#   User.create!(name: name,
+#                email: email,
+#                password: password,
+#                password_confirmation: password)
+# end
